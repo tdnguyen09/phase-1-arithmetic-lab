5b44bc0958581ef51c2f6a03ccd200032da4a162
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-01-11 16:46:18 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 20 % 3;
+let num4 = 20 % 3;
+let mod = num3 + num4;
+
+let max = Math.max(1, 2, 5,20);
